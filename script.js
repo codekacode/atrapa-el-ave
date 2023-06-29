2c6084860339ed95afea0ce9772d12b5d3b41571
Codekacode <karem.quiroz.m@gmail.com>
2023-06-28 22:39:55 -0500

    add house image and test the game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let positions = Array.from(document.querySelectorAll('.position'));
-let timeComponent = document.querySelector('#time');
-let restartComponent = document.querySelector('#restart-game');
+let positions = Array.from(document.querySelectorAll(".position"));
+let timeComponent = document.querySelector("#time");
+let startComponent = document.querySelector("#start-game");
+let restartComponent = document.querySelector("#restart-game");
 
 let scorePink = 0;
 let scoreBlue = 0;
@@ -12,55 +13,72 @@ function resetGame() {
   scoreBlue = 0;
   timeLeft = 60;
 
-   positions.forEach((position, index) => {
-     position.classList.remove('bird-pink', 'bird-blue');
-   });
+  positions.forEach((position) => {
+    position.classList.remove("bird-pink", "bird-blue");
+  });
 
-   if (timeId !== null) {
-     clearInterval(timeId);
-     timeId = null;
-   }
+  if (timeId !== null) {
+    clearInterval(timeId);
+    timeId = null;
+  }
+
+  document.getElementById("score-pink").textContent = scorePink;
+  document.getElementById("score-blue").textContent = scoreBlue;
+  timeComponent.innerText = timeLeft;
 }
 
 function startGame() {
-  timeId = setInterval(update, 1000);
+  resetGame();
   putBirds();
+  timeId = setInterval(update, 1000);
 }
 
 function update() {
   timeLeft--;
-  putBirds();
-  updateTimeDisplay();
+
+  if (timeLeft <= 0) {
+    endGame();
+  } else {
+    updateTimeDisplay();
+    putBirds();
+  }
 }
 
 function updateTimeDisplay() {
-  timeComponent.textContent = timeLeft.toString();
+  timeComponent.innerText = timeLeft;
+
+  if (timeLeft <= 10) {
+    timeComponent.style.color = "red";
+  }
 }
 
 function updateScoreDisplay() {
-  document.getElementById('score-pink').textContent = scorePink;
-  document.getElementById('score-blue').textContent = scoreBlue;
+  document.getElementById("score-pink").textContent = scorePink;
+  document.getElementById("score-blue").textContent = scoreBlue;
 }
 
 function handleClick(e) {
-  if (e.target.classList.contains('bird-pink')) {
+  if (e.target.classList.contains("bird-pink")) {
     scorePink++;
-    e.target.classList.remove('bird-pink');
-  } else if (e.target.classList.contains('bird-blue')) {
+    e.target.classList.remove("bird-pink");
+  } else if (e.target.classList.contains("bird-blue")) {
     scoreBlue++;
-    e.target.classList.remove('bird-blue');
+    e.target.classList.remove("bird-blue");
   }
   updateScoreDisplay();
 }
 
 function putBirds() {
-  positions.forEach(position => {
-    position.classList.remove('bird-pink', 'bird-blue');
+  positions.forEach((position, index) => {
+    position.classList.remove("bird-pink", "bird-blue");
+    position.style.backgroundImage = "url('./casaPajaros.png')";
     if (Math.random() < 0.33) {
       if (index % 2 === 0) {
-        position.classList.add('bird-pink');
+        position.classList.add("bird-blue");
+        position.style.backgroundImage = "url('./aveAzul.png')";
       } else {
-        position.classList.add('bird-blue');
+        position.classList.add("bird-pink");
+        position.style.backgroundImage = "url('./aveRosa.png')";
       }
     }
   });
@@ -70,9 +88,8 @@ function endGame() {
   clearInterval(timeId);
 }
 
+startComponent.addEventListener("click", startGame);
 restartComponent.addEventListener("click", startGame);
 positions.forEach((position) => {
   position.addEventListener("click", handleClick);
 });
-
-startGame();
